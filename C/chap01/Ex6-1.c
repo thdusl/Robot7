ff37f48641b54ed6983d67bc0f1797677a07df0b
thdusl <sjwkao2@gmail.com>
2025-11-03 11:55:45 +0900

    예제 6-1.c

---
+++ C/chap01/Ex6-1.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() 
+{
+	//1~10까지 출력하는 while을 작성하세요!!
+	int i = 1;
+	while (i <= 10) {
+		printf("i: %d\n", i++);
+	}
+	
+	return 0;
+}
