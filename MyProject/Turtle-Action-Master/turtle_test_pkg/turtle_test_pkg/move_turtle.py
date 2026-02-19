1d80b28457983523fcf33548ff43e4ff017afc18
thdusl <sjwkao2@gmail.com>
2026-02-19 15:55:45 +0900

    터틀봇 제어 프로그래밍 - 필요없는 코드 제거

--- MyProject/Turtle-Action-Master/turtle_test_pkg/turtle_test_pkg/move_turtle.py
+++ MyProject/Turtle-Action-Master/turtle_test_pkg/turtle_test_pkg/move_turtle.py
@@ -9,23 +9,7 @@ class Move_turtle(Node):
     super().__init__('move_turtle')
     self.qos_profile = QoSProfile(depth = 10)
     self.move_turtle = self.create_publisher(Twist, '/cmd_vel', self.qos_profile)
-    self.timer = self.create_timer(1, self.turtle_move)
     self.velocity = -1.0
-
-  def turtle_move(self):
-    msg = Twist()
-    msg.linear.x = self.velocity
-    msg.linear.y = 0.0
-    msg.linear.z = 0.0
-
-    msg.angular.x = 0.0
-    msg.angular.y = 0.0
-    msg.angular.z = 1.0
-
-    self.move_turtle.publish(msg)
-    self.velocity += 0.08
-    if self.velocity >2:
-      self.velocity = 0.0
 
 def main(args=None):
   rclpy.init(args=args)
